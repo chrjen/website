0ce66e7799aad178556c465d7bef552d0b017190
Christer Jensen <chr.code@gmail.com>
2021-09-15 22:04:25 +0200

    Add basic cards

---
+++ app/weather-app/src/components/Board.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12"> Hello </v-col>
+      <v-col v-for="(card, i) in cards" :key="i" cols="2">
+        <Card :label="card.label" :hidden="card.hidden" @click="card.hidden = !card.hidden" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Card from "./Card.vue";
+
+export default Vue.extend({
+  name: "Board",
+  components: {
+    Card,
+  },
+  data: () => ({
+    cards: [
+      {
+        label: "Today",
+        hidden: true,
+      },
+      {
+        label: "Tomorrow",
+        hidden: true,
+      },
+      {
+        label: "In 3 days",
+        hidden: true,
+      },
+      {
+        label: "Today",
+        hidden: true,
+      },
+      {
+        label: "Tomorrow",
+        hidden: true,
+      },
+      {
+        label: "In 3 days",
+        hidden: true,
+      },
+      {
+        label: "In 4 days",
+        hidden: true,
+      },
+      {
+        label: "In 5 days",
+        hidden: true,
+      },
+      {
+        label: "In 6 days",
+        hidden: true,
+      },
+      {
+        label: "In 4 days",
+        hidden: true,
+      },
+      {
+        label: "In 5 days",
+        hidden: true,
+      },
+      {
+        label: "In 6 days",
+        hidden: true,
+      },
+      {
+        label: "In 7 days",
+        hidden: true,
+      },
+      {
+        label: "In 8 days",
+        hidden: true,
+      },
+      {
+        label: "In 9 days",
+        hidden: true,
+      },
+      {
+        label: "In 7 days",
+        hidden: true,
+      },
+      {
+        label: "In 8 days",
+        hidden: true,
+      },
+      {
+        label: "In 9 days",
+        hidden: true,
+      },
+    ],
+  }),
+});
+</script>
---
+++ app/weather-app/src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card @click="$emit('click')">
+    <div :class="{ hidden: hidden }">
+      {{ label }}
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [
+      "label",
+      "hidden"
+],
+  data: () => ({
+  }),
+};
+</script>
+
+<style scoped>
+.hidden {
+    opacity: 0;
+}
+</style>

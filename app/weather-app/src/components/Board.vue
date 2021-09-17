c6a78bf3055fe4a23e7984a34ede7615e932629d
Christer Jensen <chr.code@gmail.com>
2021-09-18 01:37:21 +0200

    Filter weather into now and future sections

--- app/weather-app/src/components/Board.vue
+++ app/weather-app/src/components/Board.vue
@@ -2,10 +2,20 @@
   <v-container class="board">
     <v-row class="text-center">
       <v-col cols="12">
+        <h2>Weather now</h2>
+      </v-col>
+      <v-col cols="12">
+        Humidity {{ weatherNow.data.instant.details.relative_humidity }}%
+        <br>
+        Temp {{ weatherNow.data.instant.details.air_temperature }}°C
+      </v-col>
+
+      <v-col cols="12">
+        <h2>Weather predictions</h2>
       </v-col>
       <v-col v-for="(card, i) in cards" :key="i" cols="4">
         <Card
-          :label="card.label"
+          :time="card.time"
           :temp="card.temp"
           :hidden="card.hidden"
           @click="card.hidden = !card.hidden"
@@ -16,9 +26,9 @@
 </template>
 
 <script lang="ts">
-
 import Vue from "vue";
 import Card from "./Card.vue";
+import moment from "moment";
 
 export default Vue.extend({
   name: "Board",
@@ -26,12 +36,15 @@ export default Vue.extend({
     Card,
   },
   data: () => ({
+    weatherNow: {},
     cards: [],
   }),
   mounted() {
     this.$axios
-      .get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60&lon=11')
-      .then(resp => {
+      .get(
+        "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60&lon=11"
+      )
+      .then((resp) => {
         if (resp.status !== 200) {
           console.error(resp.statusText);
           return;
@@ -39,14 +52,22 @@ export default Vue.extend({
 
         const data = resp.data.properties;
 
-        let tmp = []
-        for (let i = 0; i < 18; i++) {  
+        let tmp = [];
+        for (let i = 0; i < 18; i++) {
           const t = data.timeseries[i];
+          const now = moment();
+          if (now.isSameOrAfter(t.time, "hour")) {
+            if (now.isSame(t.time, "hour")) {
+              console.log("Now", t);
+              this.weatherNow = t;
+            }
+            continue;
+          }
           tmp.push({
-            hidden: true,
-            label: t.time,
+            hidden: false,
+            time: t.time,
             temp: t.data.instant.details.air_temperature,
-          })
+          });
         }
         this.cards = tmp;
       });
--- app/weather-app/src/components/Card.vue
+++ app/weather-app/src/components/Card.vue
@@ -2,7 +2,7 @@
   <v-card hover color="indigo lighten-4" width="200" @click="$emit('click')">
     <div :class="{ hidden: hidden }">
       <div>
-        {{ label }}
+        {{ timeFormated }}
       </div>
       <div>{{ temp }}°C</div>
     </div>
@@ -10,9 +10,19 @@
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
-  props: ["label", "temp", "hidden"],
+  props: [
+      "time", "temp", "hidden"
+      ],
   data: () => ({}),
+  computed: {
+      timeFormated() {
+          const m = moment(this.time, true);
+          return m.format('DD/MM HH:mm');
+      }
+  }
 };
 </script>
 
